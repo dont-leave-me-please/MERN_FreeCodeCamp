bf1aedd76ac23cf384b9d6457e6cdb3a012e9869
POR SOKKHY <porsokkhy@gmail.com>
2025-11-04 14:28:36 +0800

    kaixinnn

---
+++ src/middleware/rateLimiter.js
@@ -0,0 +1,16 @@
+import ratelimit from "../config/upstash.js";
+
+const rateLimiter = async (req, res, next) => {
+  try {
+    const success = await ratelimit.limit("my-limit-key");
+    if (!success) {
+      return res.status(429).json({ Message: "Too many requests" });
+    }
+    next();
+  } catch (error) {
+    console.log("Rate Limiter Error:", error);
+    next(error);
+  }
+};
+
+export default rateLimiter;
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import notesRoutes from "./routes/noteRoutes.js";
 import connectDB from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
@@ -9,7 +10,13 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 connectDB();
+
 app.use(express.json());
+app.use(rateLimiter);
+app.use((req, res, next) => {
+  console.log("Someone made a request to:", req.url);
+  next();
+});
 
 app.use("/api/notes", notesRoutes);
 
